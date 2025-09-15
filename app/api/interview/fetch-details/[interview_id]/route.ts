f9cdd45f34182ab7c2001cde46979f3f62478717
Aryan Nayak <71493491+Aryan9131@users.noreply.github.com>
2025-09-15 20:47:44 +0530

    Update route.ts

--- app/api/interview/fetch-details/[interview_id]/route.ts
+++ app/api/interview/fetch-details/[interview_id]/route.ts
@@ -1,31 +1,21 @@
-import { supabase } from "@/integrations/supabase/client";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(
-  req: Request,
-  context: { params: { interview_id: string } }
+  req: NextRequest,
+  { params }: { params: { interview_id: string } }
 ) {
-  try {
-    const { interview_id } = context.params;
-    console.log("Fetching interviews for user:", interview_id);
+  const { interview_id } = params;
 
-    const { data: interviews, error } = await supabase
-      .from("Interviews")
-      .select(`*, Feedbacks!inner (*)`)
-      .eq("id", interview_id)
-      .order("created_at", { ascending: false });
+  // Query Supabase
+  const { data: interviews, error } = await supabase
+    .from("Interviews")
+    .select(`*, Feedbacks!inner (*)`)
+    .eq("id", interview_id)
+    .order("created_at", { ascending: false });
 
-    if (error) {
-      throw error;
-    }
-
-    console.log("Scheduled interviews in api:", interviews);
-    return NextResponse.json(interviews);
-  } catch (error) {
-    console.error("Error fetching Scheduled interviews:", error);
-    return NextResponse.json(
-      { error: "Failed to fetch Scheduled interviews" },
-      { status: 500 }
-    );
+  if (error) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
   }
+
+  return NextResponse.json(interviews);
 }
