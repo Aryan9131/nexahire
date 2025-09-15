7ea4b5ef0ada5d0b3b6648ce3e13324848fe51ae
Aryan Nayak <71493491+Aryan9131@users.noreply.github.com>
2025-09-15 20:35:00 +0530

    Update route.ts

--- app/api/interview/fetch-details/[interview_id]/route.ts
+++ app/api/interview/fetch-details/[interview_id]/route.ts
@@ -1,24 +1,31 @@
 import { supabase } from "@/integrations/supabase/client";
 import { NextResponse } from "next/server";
 
+export async function GET(
+  req: Request,
+  context: { params: { interview_id: string } }
+) {
+  try {
+    const { interview_id } = context.params;
+    console.log("Fetching interviews for user:", interview_id);
 
-export async function GET(req: Request, { params }: { params: { interview_id: string } }) {
-    try {
-        const { interview_id } = params;
-        console.log("Fetching interviews for user:", interview_id);
-        // Fetch interviews for the user from the supabase database
-        const { data: interviews, error } = await supabase
-            .from('Interviews')
-            .select(` *, Feedbacks!inner (*)`)
-            .eq('id', interview_id)
-            .order('created_at', { ascending: false });
-        if (error) {
-            throw error;
-        }
-        console.log("Scheduled interviews in api :", interviews);
-        return NextResponse.json(interviews);
-    } catch (error) {
-        console.error("Error fetching Scheduled interviews:", error);
-        return NextResponse.json({ error: "Failed to fetch Scheduled interviews" }, { status: 500 });
+    const { data: interviews, error } = await supabase
+      .from("Interviews")
+      .select(`*, Feedbacks!inner (*)`)
+      .eq("id", interview_id)
+      .order("created_at", { ascending: false });
+
+    if (error) {
+      throw error;
     }
+
+    console.log("Scheduled interviews in api:", interviews);
+    return NextResponse.json(interviews);
+  } catch (error) {
+    console.error("Error fetching Scheduled interviews:", error);
+    return NextResponse.json(
+      { error: "Failed to fetch Scheduled interviews" },
+      { status: 500 }
+    );
+  }
 }
